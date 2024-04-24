7a4975f10c6a2b33f40e49c08153d4c35a2481e3
RiseshStha <riseshstha222@gmail.com>
2024-04-24 17:31:51 +0545

    Flutter radio button

---
+++ android/app/src/main/kotlin/com/risesh/batch_32b/radio_button/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.risesh.batch_32b.radio_button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:radio_button/app/app.dart';
+
+void main() {
+  runApp(const App());
+}
